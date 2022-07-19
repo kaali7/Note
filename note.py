6b1cded7ba2d8b8d293e5c8d9115ecbeaa06db0b
Kaali <kaali1479@gmail.com>
2022-07-19 17:43:36 +0530

    basic structure of notepad are created

--- note.py
+++ note.py
@@ -1,17 +1,29 @@
 #import 
-from kivymd.app import MDApp
+# from kivymd.app import MDApp
+from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition
 
 #window size
 Window.size = (630,450)
 
 #connected to desgin file
-KV =  Builder.load_file("desgin.kv")
+Builder.load_file("desgin.kv")
 
-class Note(MDApp):
+#Screen
+class Main(Screen):
+    pass
+
+#main app
+class Note(App):
     def build(self):
-        return KV
+        
+        self.sm = ScreenManager(transition = NoTransition())
+        self.sm.add_widget(Main(name="main"))
 
+        return self.sm
+
+#run app
 App = Note()
 App.run()
